e366fd984749d1f482d0d31a22799dd36edf721b
cabarronc <92804343+cabarronc@users.noreply.github.com>
2021-12-14 11:08:24 -0600

    14/12/2021 - Cambios

--- Angular/demo/src/app/app.module.ts
+++ Angular/demo/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { CreceComponent } from './Planeacion/crece/crece.component';
 import { ProgramasPresupuestariosComponent } from './Planeacion/programas-presupuestarios/programas-presupuestarios.component';
 import { AppRoutingModule } from './app.routes';
 import { PrincipalComponent } from './Component/principal/principal.component';
+import { UsuariosComponent } from './Administracion/Usuarios/usuarios/usuarios.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [ AppComponent,
@@ -21,6 +25,7 @@ import { PrincipalComponent } from './Component/principal/principal.component';
      CreceComponent, 
      ProgramasPresupuestariosComponent, 
      PrincipalComponent, 
+     UsuariosComponent
     ],
     
   imports: [BrowserModule, 
@@ -29,7 +34,11 @@ import { PrincipalComponent } from './Component/principal/principal.component';
     SidebarModule.forRoot(),
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
+    
    ],
   providers: [],
   bootstrap: [AppComponent],
--- Angular/demo/src/app/app.routes.ts
+++ Angular/demo/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './Administracion/Usuarios/usuarios/usuarios.component';
 import { HomeComponent } from './Component/home/home.component';
 import { PrincipalComponent } from './Component/principal/principal.component';
 import { CreceComponent } from './Planeacion/crece/crece.component';
@@ -8,7 +9,8 @@ import { CreceComponent } from './Planeacion/crece/crece.component';
 const routes: Routes = [
     { path: 'crece', component:CreceComponent },
     { path: 'home', component:HomeComponent },
-    { path: 'principal', component:PrincipalComponent}
+    { path: 'principal', component:PrincipalComponent},
+    { path: 'usuarios', component:UsuariosComponent}
   ];
   @NgModule({
     imports: [RouterModule.forRoot(routes)],
