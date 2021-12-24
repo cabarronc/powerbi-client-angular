1d111f3c3d40b65e21ee2d8fa0b39240e4eabd04
cabarronc <92804343+cabarronc@users.noreply.github.com>
2021-12-24 11:50:58 -0600

    Cambios 2 24122021

---
+++ Angular/demo/src/app/Planeacion/crece-preguntas/crece-preguntas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crece-preguntas',
+  templateUrl: './crece-preguntas.component.html',
+  styleUrls: ['./crece-preguntas.component.css']
+})
+export class CrecePreguntasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
